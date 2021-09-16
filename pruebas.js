01d51614fc3fffb0a6d8be65da1cd396dd762dc2
iSebastianCabrera <jscrojas25@yahoo.es>
2021-09-15 19:11:09 -0500

    pruebas js

---
+++ pruebas.js
@@ -0,0 +1,14 @@
+
+function saludar(){
+    let respuesta = prompt("hola como estas ")
+    if (respuesta == "bien"){
+        alert("que bacano")
+    }
+    else {
+        alert("que falla")
+    } 
+}
+for (let i = 0;i<2; i++){
+    saludar()
+
+}
